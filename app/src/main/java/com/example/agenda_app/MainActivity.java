a06a03ae5d7b5b9ee0e87927565098bafa79a907
jeroenH04 <64149224+jeroenH04@users.noreply.github.com>
2021-02-11 14:40:00 +0100

    Add SplashScreen

--- app/src/main/java/com/example/agenda_app/MainActivity.java
+++ app/src/main/java/com/example/agenda_app/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
