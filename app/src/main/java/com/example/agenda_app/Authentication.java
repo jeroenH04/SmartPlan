7a4fd4dedfe12c265c471055d626919bb0904d11
V <vladgeorgescu10@yahoo.com>
2021-02-24 12:17:16 +0100

    Authentication stuffs

---
+++ app/src/main/java/com/example/agenda_app/Authentication.java
@@ -0,0 +1,41 @@
+package com.example.agenda_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class Authentication extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentication);
+
+        Button btn = (Button)findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Authentication.this, Login.class));
+            }
+        });
+
+        TextView txt = (TextView)findViewById(R.id.textView2);
+
+        txt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Authentication.this,Register.class));
+            }
+        });
+    }
+
+
+
+}
