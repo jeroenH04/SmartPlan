c6ddab481de87e6ad10ad2d15b3c3c3e8a4039c9
jeroenH04 <64149224+jeroenH04@users.noreply.github.com>
2021-03-05 12:52:04 +0100

    Add availability window

--- app/src/main/java/com/example/agenda_app/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/example/agenda_app/ui/settings/SettingsFragment.java
@@ -1,27 +1,89 @@
 package com.example.agenda_app.ui.settings;
 
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.agenda_app.R;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 public class SettingsFragment extends Fragment {
 
     private SettingsViewModel settingsViewModel;
+    private AlertDialog dialog;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         settingsViewModel =
                 ViewModelProviders.of(this).get(SettingsViewModel.class);
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View root = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        Button setAvailability = (Button) root.findViewById(R.id.setAvailability);
+        setAvailability.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createNewAvailabilityDialog();
+            }
+        });
+        return root;
+    }
+
+
+    public void createNewAvailabilityDialog() {
+        // Setup pop-up window
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this.getActivity());
+        final View taskPopUpView = getLayoutInflater().inflate(R.layout.popup_availability, null);
+        Button newTaskSave = (Button) taskPopUpView.findViewById(R.id.saveButton);
+        Button newTaskCancel = (Button) taskPopUpView.findViewById(R.id.cancelButton);
+        final EditText eText=(EditText) taskPopUpView.findViewById(R.id.newtask_deadline);
+
+        dialogBuilder.setView(taskPopUpView);
+        dialog = dialogBuilder.create();
+
+        dialog.show();
+
+        newTaskSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        newTaskCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+
     }
 }
