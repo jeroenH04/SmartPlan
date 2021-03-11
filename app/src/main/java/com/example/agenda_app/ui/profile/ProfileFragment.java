8972a8939adac3a29910b2c14b4562b4fdbfece2
Adriana <adriana09.radu@yahoo.com>
2021-03-11 17:45:19 +0100

    Cancel button functionality + save button partially works

--- app/src/main/java/com/example/agenda_app/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/agenda_app/ui/profile/ProfileFragment.java
@@ -24,22 +24,8 @@ public class ProfileFragment extends Fragment {
     private AlertDialog dialog;
     private AlertDialog dialogShow;
 
-   // private ProfileViewModel profileViewModel;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        /*profileViewModel =
-                ViewModelProviders.of(this).get(ProfileViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_profile, container, false);
-        final TextView textView = root.findViewById(R.id.text_profile);
-
-        profileViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root; */
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
 
         Button btnResetPass = (Button) root.findViewById(R.id.buttonResetPass);
@@ -57,13 +43,46 @@ public class ProfileFragment extends Fragment {
     @RequiresApi(api = Build.VERSION_CODES.N)
     public void createNewResetPassDialog() {
 
+        //Setup popup window
         AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this.getActivity());
         final View resetPassPopUpView = getLayoutInflater().inflate(R.layout.reset_pw_popup, null);
+        Button btnSave = (Button) resetPassPopUpView.findViewById(R.id.button_Save);
+        Button btnCancel = (Button) resetPassPopUpView.findViewById(R.id.button_Cancel);
+        final TextView editOldPass = (TextView) resetPassPopUpView.findViewById(R.id.edit_passOld);
+        final TextView editNewPass = (TextView) resetPassPopUpView.findViewById(R.id.edit_passNew);
 
         dialogBuilder.setView(resetPassPopUpView);
         dialogShow = dialogBuilder.create();
         dialogShow.show();
 
+        //Set the cancel button to close the dialog
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialogShow.dismiss();
+            }
+        });
+
+        //Set the save button to save the new password
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String oldPass = editOldPass.getText().toString().trim();
+                String newPass = editNewPass.getText().toString().trim();
+
+                //If field for old password is left empty
+                if (oldPass.isEmpty()) {
+                    editOldPass.requestFocus();
+                    editOldPass.setError("Old password field cannot be left empty!");
+                } else if (newPass.isEmpty()) { //If field for new password is left empty
+                    editNewPass.requestFocus();
+                    editNewPass.setError("New password field cannot be left empty!");
+                } else if (newPass.equals(oldPass)) { //If the new password added is the same with the old password
+                    editNewPass.requestFocus();
+                    editNewPass.setError("New password cannot be the same with the old password!");
+                }
+            }
+        });
 
     }
 }
