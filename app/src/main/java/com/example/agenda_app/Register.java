39d82161c5d694ab2d34394eeb114abbc4232c69
AkukuYellowPenguin <64163304+AkukuYellowPenguin@users.noreply.github.com>
2021-03-12 09:28:27 +0100

    Add files via upload

--- app/src/main/java/com/example/agenda_app/Register.java
+++ app/src/main/java/com/example/agenda_app/Register.java
@@ -21,6 +21,11 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
 
@@ -31,6 +36,7 @@ public class Register extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         Button btn = (Button)findViewById(R.id.regBtn);
+        Button test = (Button)findViewById(R.id.test);
         TextView signIn = (TextView)findViewById(R.id.signIn);
         final FirebaseAuth fAuth;
         final ProgressBar pBar;
@@ -42,6 +48,8 @@ public class Register extends AppCompatActivity {
         pBar = findViewById(R.id.pBar);
         fAuth = FirebaseAuth.getInstance();
         final String TAG = "Register";
+
+
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,9 +82,11 @@ public class Register extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
 
+                            createUser(username);
+
                             // verify email
-                            FirebaseUser user = fAuth.getCurrentUser();
-                            user.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
+                            FirebaseUser uSer = fAuth.getCurrentUser();
+                            uSer.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
                                 @Override
                                 public void onSuccess(Void aVoid) {
                                     Toast.makeText(Register.this,"Verification mail sent",Toast.LENGTH_SHORT).show();
@@ -110,8 +120,44 @@ public class Register extends AppCompatActivity {
             }
         });
 
+        test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+            }
+        });
 
 
     }
+
+    public void createUser(String name){
+
+        //Firestore entry
+        final String TAG = "Register";
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        // Create a new user with a first and last name
+        Map<String, Object> user = new HashMap<>();
+        user.put("Username", name);
+        user.put("Studymode", "Off");
+        user.put("Tasks", "No");
+
+
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+
+    }
+
 }
