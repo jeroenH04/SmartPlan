68383b18fe1e0c8ee611c91848ea12cd4fcbc10d
Pingu <nhq5npj89bjs@hotmail.com>
2021-03-16 02:30:20 +0100

    Accelerometer and Gyroscope Class created and implemented in MainActivity for now. Changed position of buttons in fragment_planning.xml because we couldn't see it.

--- app/src/main/java/com/example/agenda_app/MainActivity.java
+++ app/src/main/java/com/example/agenda_app/MainActivity.java
@@ -1,11 +1,12 @@
 package com.example.agenda_app;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
+/*import com.google.firebase.firestore.DocumentReference;*/
+/*import com.google.firebase.firestore.FirebaseFirestore;*/
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -17,11 +18,39 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Accelerometer accelerometer;
+    private Gyroscope gyroscope;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
+
+        accelerometer = new Accelerometer(this);
+        gyroscope = new Gyroscope(this);
+
+        accelerometer.setListener(new Accelerometer.Listener() {
+            @Override
+            public void onTranslation(float tx, float ty, float tz) {
+                if (tx > 1.0f) {
+
+                } else if (tx < -1.0f) {
+
+                }
+            }
+        });
+
+        gyroscope.setListener(new Gyroscope.Listener() {
+            @Override
+            public void onRotation(float rx, float ry, float rz) {
+                if (rx > 1.0f) {
+
+                } else if (rx < -1.0f) {
+
+                }
+            }
+        });
 
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -31,6 +60,22 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        accelerometer.register();
+        gyroscope.register();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        accelerometer.unregister();
+        gyroscope.unregister();
     }
 
     public void logout(View view){
