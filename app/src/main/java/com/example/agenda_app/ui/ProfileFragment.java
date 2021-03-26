97d7fd01238ba70014590f702c951528526557da
jeroenH04 <64149224+jeroenH04@users.noreply.github.com>
2021-03-26 13:22:55 +0100

    Implement old password checker

--- app/src/main/java/com/example/agenda_app/ui/ProfileFragment.java
+++ app/src/main/java/com/example/agenda_app/ui/ProfileFragment.java
@@ -18,6 +18,8 @@ import androidx.fragment.app.Fragment;
 import com.example.agenda_app.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -110,9 +112,9 @@ public class ProfileFragment extends Fragment {
             @Override
             public void onClick(final View v) {
                 String oldPass = editOldPass.getText().toString().trim();
-                String newPass = editNewPass.getText().toString().trim();
+                final String newPass = editNewPass.getText().toString().trim();
 
-                //If field for old password is left empty
+                // If field for old password is left empty
                 if (oldPass.isEmpty()) {
                     editOldPass.requestFocus();
                     editOldPass.setError("Old password field cannot be left "
@@ -123,31 +125,50 @@ public class ProfileFragment extends Fragment {
                             + " empty!");
                 } else if (newPass.equals(oldPass)) {
                     editNewPass.requestFocus();
-                    editNewPass.setError("New password cannot be the same with"
+                    editNewPass.setError("New password cannot be the same as"
                             + " the old password!");
+                } else if (newPass.length() < 6) {
+                        editNewPass.requestFocus();
+                        editNewPass.setError("The new password should contain"
+                                + " at least 6 characters");
                 } else {
-                    user.updatePassword(newPass).addOnCompleteListener(
-                            new OnCompleteListener<Void>() {
+                    AuthCredential credential = EmailAuthProvider
+                            .getCredential(user.getEmail(), oldPass);
+
+                    user.reauthenticate(credential)
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
                                 @Override
-                                public void onComplete(@NonNull final Task<Void> task) {
+                                public void onComplete(@NonNull Task<Void> task) {
                                     if (task.isSuccessful()) {
-                                        Log.d(TAG,
-                                                "Password updated " +
-                                                        "successfully!");
-                                        Toast.makeText(getActivity(),
-                                                "Password replaced",
-                                                Toast.LENGTH_SHORT).show();
+                                        user.updatePassword(newPass)
+                                                .addOnCompleteListener(
+                                                        new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if (task.isSuccessful()) {
+                                                    Toast.makeText(getActivity(),
+                                                            "Password updated",
+                                                            Toast.LENGTH_SHORT).show();
+                                                            dialogShow.dismiss();
+                                                } else {
+                                                    Toast.makeText(getActivity(),
+                                                            "Error! Password not updated",
+                                                            Toast.LENGTH_SHORT).show();
+                                                }
+                                            }
+                                        });
                                     } else {
-                                        Log.d(TAG, "Error password not " +
-                                                "updated!");
                                         Toast.makeText(getActivity(),
-                                                "Password not replaced",
+                                                "Error! Old password does not match!",
                                                 Toast.LENGTH_SHORT).show();
                                     }
                                 }
                             });
-                    dialogShow.dismiss();
                 }
+
+//
+
+             //   }
             }
         });
     }
